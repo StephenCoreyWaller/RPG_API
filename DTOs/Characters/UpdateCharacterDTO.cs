3073af1b5ca4a737f062275bb89e763134051f54
stephen waller <stephenwaller@stephens-MacBook-Pro.local>
2020-08-08 12:48:54 -0600

    Finish CRUD user character operations

--- DTOs/Characters/UpdateCharacterDTO.cs
+++ DTOs/Characters/UpdateCharacterDTO.cs
@@ -4,13 +4,13 @@ namespace WebAPI_RPG.DTOs.Characters
 {
     public class UpdateCharacterDTO
     {
-        public string Name { get; set; } 
+        public string? Name { get; set; } 
         public int Id { get; set; }
-        public int Health { get; set; } 
-        public int Magic { get; set; } 
-        public int Strength { get; set; } 
-        public int Dexterity { get; set; } 
-        public int Spirit { get; set; } 
-        public RPG_Class Class { get; set; } 
+        public int? Health { get; set; } 
+        public int? Magic { get; set; } 
+        public int? Strength { get; set; } 
+        public int? Dexterity { get; set; } 
+        public int? Spirit { get; set; } 
+        public RPG_Class? Class { get; set; } 
     }
 }
--- Services/CharacterService/CharacterService.cs
+++ Services/CharacterService/CharacterService.cs
@@ -45,30 +45,37 @@ namespace WebAPI_RPG.Services.CharacterService
         public async Task<ServiceWrapper<GetCharacterDTO>> GetCharacterByNameService(int id)
         {
             ServiceWrapper<GetCharacterDTO> _wrapper = new ServiceWrapper<GetCharacterDTO>();
-            Character character = await _context.Characters.FirstAsync(c => c.Id == id);
-            _wrapper.Data = _mapper.Map<GetCharacterDTO>(character);
+
+            try{
+
+                Character character = await _context.Characters.Where(c => c.User.id == GetUserId()).FirstAsync(c => c.Id == id);
+                _wrapper.Data = _mapper.Map<GetCharacterDTO>(character);
+
+            }catch(Exception ex){
+
+                _wrapper.DidSend = false; 
+                _wrapper.Message = ex.Message; 
+            }
             return _wrapper;
         }
-
         public async Task<ServiceWrapper<GetCharacterDTO>> UpdateCharacter(UpdateCharacterDTO character)
         {
             ServiceWrapper<GetCharacterDTO> _wrapper = new ServiceWrapper<GetCharacterDTO>();
             try
             {
-                Character updatedChar = await _context.Characters.FirstAsync(c => c.Id == character.Id);
-                updatedChar.Name = character.Name;
-                updatedChar.Health = character.Health;
-                updatedChar.Magic = character.Magic;
-                updatedChar.Spirit = character.Dexterity;
-                updatedChar.Strength = character.Strength;
-                updatedChar.Class = character.Class;
+                Character updatedChar = await _context.Characters.Where(u => u.User.id == GetUserId()).FirstAsync(c => c.Id == character.Id);
+                updatedChar.Name = character.Name ?? updatedChar.Name;
+                updatedChar.Health = character.Health ?? updatedChar.Health;
+                updatedChar.Magic = character.Magic ?? updatedChar.Magic;
+                updatedChar.Spirit = character.Dexterity ?? updatedChar.Dexterity;
+                updatedChar.Strength = character.Strength ?? updatedChar.Strength;
+                updatedChar.Class = character.Class ?? updatedChar.Class;
                 _context.Characters.Update(updatedChar);
                 await _context.SaveChangesAsync();
                 _wrapper.Data = _mapper.Map<GetCharacterDTO>(await _context.Characters.FirstAsync(c => c.Id == updatedChar.Id));
             }
             catch (Exception ex)
             {
-
                 _wrapper.DidSend = false;
                 _wrapper.Message = ex.Message;
             }
@@ -80,10 +87,10 @@ namespace WebAPI_RPG.Services.CharacterService
 
             try
             {
-                Character character = await _context.Characters.FirstAsync(c => c.Id == id);
+                Character character = await _context.Characters.Where(u => u.User.id == GetUserId()).FirstAsync(c => c.Id == id);
                 _context.Characters.Remove(character);
                 await _context.SaveChangesAsync();
-                List<Character> characters = await _context.Characters.ToListAsync();
+                List<Character> characters = await _context.Characters.Where(u => u.User.id == GetUserId()).ToListAsync();
                 _wrapper.Data = characters.Select(c => _mapper.Map<GetCharacterDTO>(c)).ToList();
             }
             catch (Exception ex)
